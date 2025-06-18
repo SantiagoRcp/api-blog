08665865124f5148d811bd645013651e369af088
SantiagoRcp <santiagorcp.9854@gmail.com>
2025-06-17 22:07:06 -0600

    Agregar archivos de configuración inicial para el proyecto API RESTful de blogs, incluyendo .gitignore, package.json, package-lock.json, tsconfig.json, y la estructura básica de la aplicación en TypeScript con Express y Sequelize.

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+export default app;
